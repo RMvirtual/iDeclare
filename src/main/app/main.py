a82c139679b65c0ddc728e7b2a4dbb36a82b519b
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-10-12 16:11:34 +0100

    Completed rough version of command line checker.

--- src/main/app/main.py
+++ src/main/app/main.py
@@ -2,7 +2,20 @@ from src.main.tss import connection
 
 
 def main():
-    print("Please input an eori number: ")
+    eori_candidate = input(
+        "Please input an EORI number to check type \"exit\": ")
+
+    if eori_candidate.lower() == "exit":
+        exit()
+
+    elif connection.is_eori_valid(eori_candidate):
+        print("EORI number " + eori_candidate + " is valid.")
+        input("\nPress any key to continue.")
+        main()
+
+    else:
+        print("EORI number " + eori_candidate + " is INVALID.")
+        main()
 
 
 if __name__ == '__main__':
