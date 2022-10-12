d56a1289532c4595dc4ca33a9c41b8a952d19e2f
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-10-12 17:14:44 +0100

    More responsive CLI.

--- src/main/app/main.py
+++ src/main/app/main.py
@@ -1,9 +1,10 @@
+import os
 from src.main.tss import connection
 
 
 def main():
     eori_candidate = input(
-        "Please input an EORI number to check type \"exit\": ")
+        "Please input an EORI number to check or type \"exit\": ")
 
     if eori_candidate.lower() == "exit":
         exit()
@@ -11,10 +12,13 @@ def main():
     elif connection.is_eori_valid(eori_candidate):
         print("EORI number " + eori_candidate + " is valid.")
         input("\nPress any key to continue.")
+        os.system("cls")
         main()
 
     else:
         print("EORI number " + eori_candidate + " is INVALID.")
+        input("\nPress any key to continue.")
+        os.system("cls")
         main()
 
 
