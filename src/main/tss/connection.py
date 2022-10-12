0113d2ec040095d80b936a9cc2727d62ec64dfd1
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-10-12 11:30:39 +0100

    Removed unused variable left over in read from copying the create method.

--- src/main/tss/connection.py
+++ src/main/tss/connection.py
@@ -6,29 +6,6 @@ def read_dummy_consignment() -> None:
     login = credentials.user_credentials()
     environment = api_environments.environments().TEST
 
-    example_data = """{
-        "op_type": "create",
-        "declaration_number": "",
-        "movement_type": "3",
-        "identity_no_of_transport": "xy12345",
-        "nationality_of_transport": "GB",
-        "conveyance_ref": "",
-        "arrival_date_time": "13/10/2022 10:00:00",
-        "arrival_port": "GBAUBELBELBEL",
-        "place_of_loading": "Birkenhead",
-        "place_of_unloading": "Belfast",
-        "seal_number": "s123456",
-        "route": "gb-ni",
-        "transport_charges": "Y",
-        "carrier_eori": "XI123456789012",
-        "carrier_name": "",
-        "carrier_street_number": "",
-        "carrier_city": "",
-        "carrier_postcode": "",
-        "carrier_country": "",
-        "haulier_eori": ""
-    }"""
-
     response = requests.get(
         url="https://" + environment
             + "/api/x_fhmrc_tss_api/v1/tss_api/headers?"
