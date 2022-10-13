41f3e47ce2c059de82c07b499ccc3a8fddf307c4
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-10-13 15:07:00 +0100

    Better method for creating the tss URLs

--- src/main/tss/url.py
+++ src/main/tss/url.py
@@ -2,9 +2,9 @@ from src.main.file_system.api_environments import ApiEnvironment
 
 
 def tss_url(configuration: ApiEnvironment, resource: str) -> str:
-    return (
-            "https://"
-            + configuration.domain
-            + "/api/x_fhmrc_tss_api/v1/tss_api/"
-            + configuration.resources[resource]
-        )
+    return "https://" + "/".join((
+        configuration.domain,
+        "api/x_fhmrc_tss_api/v1/tss_api",
+        configuration.resources[resource]
+    ))
+
