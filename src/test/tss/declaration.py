afa401706e5cb81b056b74649f36508150803ec1
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-10-13 16:25:31 +0100

    Refactored DeclarationHeader API Call tests.

--- src/test/tss/declaration.py
+++ src/test/tss/declaration.py
@@ -1,34 +1,38 @@
+import re
 import unittest
-from src.main.tss.declarations.declaration_header import DeclarationHeader
-from src.main.tss.api.environment.environments import TestEnvironment
+
 from src.main.tss.api.calls.declaration import DeclarationHeaderApiCall
+from src.main.tss.api.environment.environments import TestEnvironment
+from src.main.tss.declarations.declaration_header import DeclarationHeader
 
 
-class TestTssDeclaration(unittest.TestCase):
+class TestDeclarationHeaderApiCall(unittest.TestCase):
     def setUp(self):
         self._environment = TestEnvironment()
         self._graylaw_eori = self._environment.eori_no
+        self._declaration_header = DeclarationHeader()
+        self._dummy_draft = self._environment.draft_declaration
 
     def test_should_create_declaration(self) -> None:
         api_call = DeclarationHeaderApiCall(self._environment)
-        header = DeclarationHeader()
-        ens_reference = api_call.create(header)
+        ens_no = api_call.create(self._declaration_header)
 
-        self.assertTrue(ens_reference.startswith("ENS"))
+        ens_reference_received = bool(re.fullmatch(r"ENS\d{15}", ens_no))
+        self.assertTrue(ens_reference_received)
 
     def test_should_find_declaration_ens_is_draft(self) -> None:
         api_call = DeclarationHeaderApiCall(self._environment)
+        ens_draft_exists = api_call.is_ens_no_draft(self._dummy_draft)
 
-        self.assertTrue(api_call.is_ens_no_draft(
-            self._environment.draft_declaration))
+        self.assertTrue(ens_draft_exists)
 
     def test_should_cancel_declaration(self) -> None:
         api_call = DeclarationHeaderApiCall(self._environment)
-        header = DeclarationHeader()
-        ens_reference = api_call.create(header)
+        ens_reference = api_call.create(self._declaration_header)
         report = api_call.cancel(ens_reference)
 
-        self.assertEqual("SUCCESS", report["result"]["process_message"])
+        success_reported = (report["result"]["process_message"] == "SUCCESS")
+        self.assertTrue(success_reported)
 
 
 if __name__ == '__main__':
