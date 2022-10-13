7e92f74c5de16d21dd5be0dd3274bfbb4112a6aa
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-10-13 10:28:38 +0100

    Refactored TSS API test.

--- src/test/tss/api.py
+++ src/test/tss/api.py
@@ -6,14 +6,15 @@ from src.main.file_system.api_environments import TestEnvironment
 class TestTssApi(unittest.TestCase):
     def setUp(self):
         self._environment = TestEnvironment()
-        self._graylaw_eori = self._environment.eori_no
-        self._api = TssApi(self._environment)
 
     def test_should_check_eori_number_is_valid(self):
-        self.assertTrue(self._api.is_eori_valid(self._graylaw_eori))
+        api = TssApi(self._environment)
+
+        valid_eori = self._environment.eori_no
+        self.assertTrue(api.is_eori_valid(valid_eori))
 
         invalid_eori = "XI123456798012"
-        self.assertFalse(self._api.is_eori_valid(invalid_eori))
+        self.assertFalse(api.is_eori_valid(invalid_eori))
 
 
 if __name__ == '__main__':
