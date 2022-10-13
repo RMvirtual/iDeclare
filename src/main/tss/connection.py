5aba49157d65460f3a6af602b386868a71fec167
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-10-13 09:31:22 +0100

    Merged tss functions into a TSSApi Class.

--- src/main/tss/connection.py
+++ src/main/tss/connection.py
@@ -1,178 +1,182 @@
 import requests
-from src.main.file_system import credentials, api_environments
+from src.main.file_system import api_environments
+from src.main.file_system.api_environments import ApiEnvironment
 
 
-def is_eori_valid(eori_number: str) -> bool:
-    dummy_declaration = create_declaration()
-    report = create_consignment(dummy_declaration, eori_number)
+class TssApi:
+    def __init__(self, configuration: ApiEnvironment):
+        self._configuration = configuration
 
-    success = report["result"]["process_message"] == "SUCCESS"
+    def is_eori_valid(self, eori_number: str) -> bool:
+        dummy_declaration = self.create_declaration()
+        report = self.create_consignment(dummy_declaration, eori_number)
 
-    if success:
-        cancel_consignment(report["result"]["reference"])
+        success = report["result"]["process_message"] == "SUCCESS"
 
-    cancel_declaration(dummy_declaration)
+        if success:
+            self.cancel_consignment(report["result"]["reference"])
 
-    return success
+        self.cancel_declaration(dummy_declaration)
 
+        return success
 
-def read_consignment(consignment_reference: str) -> str:
-    environment = api_environments.TestEnvironment()
+    def read_consignment(self, consignment_reference: str) -> str:
+        environment = api_environments.TestEnvironment()
 
-    response = requests.get(
-        url="https://" + environment.DOMAIN
-            + "/api/x_fhmrc_tss_api/v1/tss_api/consignments",
-        auth=(environment.USER_NAME, environment.PASSWORD),
-        params="reference=" + consignment_reference
-            + "&fields=importer_eori"
-    )
+        response = requests.get(
+            url="https://" + environment.DOMAIN
+                + "/api/x_fhmrc_tss_api/v1/tss_api/consignments",
+            auth=(environment.USER_NAME, environment.PASSWORD),
+            params="reference=" + consignment_reference
+                + "&fields=importer_eori"
+        )
 
-    return response.json()["result"]["importer_eori"]
-
-
-def cancel_consignment(dec_number: str) -> dict[str, str]:
-    environment = api_environments.TestEnvironment()
-
-    example_data = {
-        "op_type": "cancel",
-        "consignment_number": dec_number
-    }
-
-    response = requests.post(
-        url="https://" + environment.DOMAIN +
-            "/api/x_fhmrc_tss_api/v1/tss_api/consignments",
-        auth=(environment.USER_NAME, environment.PASSWORD),
-        json=example_data
-    )
-
-    return response.json()
+        return response.json()["result"]["importer_eori"]
 
 
-def create_consignment(
-        ens_number: str, importer_eori_number: str) -> dict[str, str]:
-    environment = api_environments.TestEnvironment()
+    def cancel_consignment(self, dec_number: str) -> dict[str, str]:
+        environment = api_environments.TestEnvironment()
 
-    example_data = {
-        "op_type": "create",
-        "declaration_number": ens_number,
-        "consignment_number": "",
-        "goods_description": "DUMMY TO CHECK EORI",
-        "trader_reference": "",
-        "transport_document_number": "DUMMY JOB",
-        "controlled_goods": "no",
-        "goods_domestic_status": "",
-        "supervising_customs_office": "",
-        "customs_warehouse_identifier": "",
-        "ducr": "",
-        "consignor_eori": "",
-        "consignor_name": "DUMMY",
-        "consignor_street_number": "DUMMY",
-        "consignor_city": "DUMMY",
-        "consignor_postcode": "BT1 1AA",
-        "consignor_country": "GB",
-        "consignee_eori": "",
-        "consignee_name": "DUMMY",
-        "consignee_street_number": "DUMMY",
-        "consignee_city": "DUMMY",
-        "consignee_postcode": "DUMMY",
-        "consignee_country": "GB",
-        "importer_eori": importer_eori_number,
-        "exporter_eori": "",
-        "exporter_name": "DUMMY",
-        "exporter_street_number": "DUMMY",
-        "exporter_city": "DUMMY",
-        "exporter_postcode": "DUMMY",
-        "exporter_country": "GB",
-        "header_previous_document": [
-            {
-                "op_type": "create",
-                "previous_document_class": "",
-                "previous_document_type": "",
-                "previous_document_ref": ""
-            }
-        ],
-        "holder_of_authorisation": [
-            {
-                "op_type": "create",
-                "auth_role_id": "",
-                "auth_type_code": ""
-            }
-        ]
-    }
+        example_data = {
+            "op_type": "cancel",
+            "consignment_number": dec_number
+        }
 
-    response = requests.post(
-        url="https://" + environment.DOMAIN +
-            "/api/x_fhmrc_tss_api/v1/tss_api/consignments",
-        auth=(environment.USER_NAME, environment.PASSWORD),
-        json=example_data
-    )
+        response = requests.post(
+            url="https://" + environment.DOMAIN +
+                "/api/x_fhmrc_tss_api/v1/tss_api/consignments",
+            auth=(environment.USER_NAME, environment.PASSWORD),
+            json=example_data
+        )
 
-    return response.json()
+        return response.json()
 
 
-def read_declaration() -> None:
-    environment = api_environments.TestEnvironment()
+    def create_consignment(self,
+            ens_number: str, importer_eori_number: str) -> dict[str, str]:
+        environment = api_environments.TestEnvironment()
 
-    response = requests.get(
-        url="https://" + environment.DOMAIN
-            + "/api/x_fhmrc_tss_api/v1/tss_api/headers",
-        auth=(environment.USER_NAME, environment.PASSWORD),
-        params="reference=ENS000000000405352"
-            + "&fields=status,arrival_port,seal_number,route,carrier_eori"
-    )
+        example_data = {
+            "op_type": "create",
+            "declaration_number": ens_number,
+            "consignment_number": "",
+            "goods_description": "DUMMY TO CHECK EORI",
+            "trader_reference": "",
+            "transport_document_number": "DUMMY JOB",
+            "controlled_goods": "no",
+            "goods_domestic_status": "",
+            "supervising_customs_office": "",
+            "customs_warehouse_identifier": "",
+            "ducr": "",
+            "consignor_eori": "",
+            "consignor_name": "DUMMY",
+            "consignor_street_number": "DUMMY",
+            "consignor_city": "DUMMY",
+            "consignor_postcode": "BT1 1AA",
+            "consignor_country": "GB",
+            "consignee_eori": "",
+            "consignee_name": "DUMMY",
+            "consignee_street_number": "DUMMY",
+            "consignee_city": "DUMMY",
+            "consignee_postcode": "DUMMY",
+            "consignee_country": "GB",
+            "importer_eori": importer_eori_number,
+            "exporter_eori": "",
+            "exporter_name": "DUMMY",
+            "exporter_street_number": "DUMMY",
+            "exporter_city": "DUMMY",
+            "exporter_postcode": "DUMMY",
+            "exporter_country": "GB",
+            "header_previous_document": [
+                {
+                    "op_type": "create",
+                    "previous_document_class": "",
+                    "previous_document_type": "",
+                    "previous_document_ref": ""
+                }
+            ],
+            "holder_of_authorisation": [
+                {
+                    "op_type": "create",
+                    "auth_role_id": "",
+                    "auth_type_code": ""
+                }
+            ]
+        }
 
-    return response.json()
+        response = requests.post(
+            url="https://" + environment.DOMAIN +
+                "/api/x_fhmrc_tss_api/v1/tss_api/consignments",
+            auth=(environment.USER_NAME, environment.PASSWORD),
+            json=example_data
+        )
+
+        return response.json()
 
 
-def cancel_declaration(ens_number: str) -> dict[str, str]:
-    environment = api_environments.TestEnvironment()
+    def read_declaration(self) -> None:
+        environment = api_environments.TestEnvironment()
 
-    example_data = {
-        "op_type": "cancel",
-        "declaration_number": ens_number
-    }
+        response = requests.get(
+            url="https://" + environment.DOMAIN
+                + "/api/x_fhmrc_tss_api/v1/tss_api/headers",
+            auth=(environment.USER_NAME, environment.PASSWORD),
+            params="reference=ENS000000000405352"
+                + "&fields=status,arrival_port,seal_number,route,carrier_eori"
+        )
 
-    response = requests.post(
-        url="https://" + environment.DOMAIN
-            + "/api/x_fhmrc_tss_api/v1/tss_api/headers",
-        auth=(environment.USER_NAME, environment.PASSWORD),
-        json=example_data
-    )
-
-    return response.json()
+        return response.json()
 
 
-def create_declaration() -> str:
-    environment = api_environments.TestEnvironment()
+    def cancel_declaration(self, ens_number: str) -> dict[str, str]:
+        environment = api_environments.TestEnvironment()
 
-    example_data = {
-        "op_type": "create",
-        "declaration_number": "",
-        "movement_type": "3",
-        "identity_no_of_transport": "xy12345",
-        "nationality_of_transport": "GB",
-        "conveyance_ref": "",
-        "arrival_date_time": "13/10/2022 10:00:00",
-        "arrival_port": "GBAUBELBELBEL",
-        "place_of_loading": "Birkenhead",
-        "place_of_unloading": "Belfast",
-        "seal_number": "s123456",
-        "route": "gb-ni",
-        "transport_charges": "Y",
-        "carrier_eori": "XI123456789012",
-        "carrier_name": "",
-        "carrier_street_number": "",
-        "carrier_city": "",
-        "carrier_postcode": "",
-        "carrier_country": "",
-        "haulier_eori": ""
-    }
+        example_data = {
+            "op_type": "cancel",
+            "declaration_number": ens_number
+        }
 
-    response = requests.post(
-        url="https://" + environment.DOMAIN +
-            "/api/x_fhmrc_tss_api/v1/tss_api/headers?",
-        auth=(environment.USER_NAME, environment.PASSWORD),
-        json=example_data
-    )
+        response = requests.post(
+            url="https://" + environment.DOMAIN
+                + "/api/x_fhmrc_tss_api/v1/tss_api/headers",
+            auth=(environment.USER_NAME, environment.PASSWORD),
+            json=example_data
+        )
 
-    return response.json()["result"]["reference"]
+        return response.json()
+
+
+    def create_declaration(self) -> str:
+        environment = api_environments.TestEnvironment()
+
+        example_data = {
+            "op_type": "create",
+            "declaration_number": "",
+            "movement_type": "3",
+            "identity_no_of_transport": "xy12345",
+            "nationality_of_transport": "GB",
+            "conveyance_ref": "",
+            "arrival_date_time": "13/10/2022 10:00:00",
+            "arrival_port": "GBAUBELBELBEL",
+            "place_of_loading": "Birkenhead",
+            "place_of_unloading": "Belfast",
+            "seal_number": "s123456",
+            "route": "gb-ni",
+            "transport_charges": "Y",
+            "carrier_eori": "XI123456789012",
+            "carrier_name": "",
+            "carrier_street_number": "",
+            "carrier_city": "",
+            "carrier_postcode": "",
+            "carrier_country": "",
+            "haulier_eori": ""
+        }
+
+        response = requests.post(
+            url="https://" + environment.DOMAIN +
+                "/api/x_fhmrc_tss_api/v1/tss_api/headers?",
+            auth=(environment.USER_NAME, environment.PASSWORD),
+            json=example_data
+        )
+
+        return response.json()["result"]["reference"]
--- src/test/tss/connection.py
+++ src/test/tss/connection.py
@@ -1,47 +1,51 @@
 import unittest
 from src.main.tss import connection
-from src.main.file_system.credentials import user_credentials
+from src.main.file_system.api_environments import TestEnvironment
 
 
 class TestTssApi(unittest.TestCase):
+    def setUp(self):
+        self._environment = TestEnvironment()
+        self._api = connection.TssApi(self._environment)
+
     def test_should_check_eori_number_is_valid(self):
-        valid_eori = user_credentials().graylaw_eori_number
-        self.assertTrue(connection.is_eori_valid(valid_eori))
+        self.assertTrue(self._api.is_eori_valid(self._environment.EORI_NO))
 
         invalid_eori = "XI123456798012"
-        self.assertFalse(connection.is_eori_valid(invalid_eori))
+        self.assertFalse(self._api.is_eori_valid(invalid_eori))
 
     def test_should_create_declaration(self) -> None:
-        ens_reference = connection.create_declaration()
+        ens_reference = self._api.create_declaration()
         self.assertTrue(ens_reference.startswith("ENS"))
 
     def test_should_cancel_declaration(self) -> None:
-        ens_reference = connection.create_declaration()
-        report = connection.cancel_declaration(ens_reference)
+        ens_reference = self._api.create_declaration()
+        report = self._api.cancel_declaration(ens_reference)
         self.assertEqual("SUCCESS", report["result"]["process_message"])
 
     def test_should_create_consignment(self) -> None:
-        eori_number = user_credentials().graylaw_eori_number
         ens_number = "ENS000000000405352"
-        report = connection.create_consignment(ens_number, eori_number)
+
+        report = self._api.create_consignment(
+            ens_number, self._environment.EORI_NO)
 
         self.assertEqual("SUCCESS", report["result"]["process_message"])
         self.assertTrue(report["result"]["reference"].startswith("DEC"))
 
     def test_should_read_consignment_eori(self) -> None:
         consignment = "DEC000000001010576"
-        eori_number = connection.read_consignment(consignment)
-        correct_eori_number = user_credentials().graylaw_eori_number
+        eori_number = self._api.read_consignment(consignment)
 
-        self.assertEqual(correct_eori_number, eori_number)
+        self.assertEqual(self._environment.EORI_NO, eori_number)
 
     def test_should_delete_consignment(self) -> None:
-        eori_number = user_credentials().graylaw_eori_number
         ens_number = "ENS000000000405352"
-        report = connection.create_consignment(ens_number, eori_number)
+
+        report = self._api.create_consignment(
+            ens_number, self._environment.EORI_NO)
 
         dec_reference = report["result"]["reference"]
-        cancel_report = connection.cancel_consignment(dec_reference)
+        cancel_report = self._api.cancel_consignment(dec_reference)
 
         self.assertEqual(
             "SUCCESS", cancel_report["result"]["process_message"])
