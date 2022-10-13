d3f775c16b5923ee18c05ecf185057713615a8e2
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-10-13 11:52:52 +0100

    Refactoring. Fix for arrival date on create_declaration (needs a valid future date).

--- src/main/file_system/api_environments.py
+++ src/main/file_system/api_environments.py
@@ -10,12 +10,14 @@ class ApiEnvironment:
     user_name: str = ""
     password: str = ""
     eori_no: str = ""
+    draft_declaration: str = ""
 
 
 class TestEnvironment(ApiEnvironment):
     def __init__(self):
         super().__init__()
         self.domain = _deserialise_environments_file()["TEST"]
+        self.draft_declaration = _deserialise_draft_declarations_file()["TEST"]
         self._initialise_login_credentials()
 
     def _initialise_login_credentials(self):
@@ -29,17 +31,21 @@ class ProductionEnvironment(ApiEnvironment):
     def __init__(self):
         super().__init__()
         self.domain = _deserialise_environments_file()["PROD"]
+        self.draft_declaration = _deserialise_draft_declarations_file()["PROD"]
         self._initialise_login_credentials()
 
     def _initialise_login_credentials(self):
         login_credentials = credentials.user_credentials()
         self.user_name = login_credentials.user_name
-        self.password = login_credentials.test_environment_password
+        self.password = login_credentials.production_environment_password
         self.eori_no = login_credentials.graylaw_eori_number
 
 
 def _deserialise_environments_file() -> dict[str, str]:
-    environments_file = runfiles.load_path("config/api/environments.json")
+    with open(runfiles.load_path("config/api/environments.json")) as stream:
+        return json.load(stream)
 
-    with open(environments_file) as file_stream:
-        return json.load(file_stream)
+
+def _deserialise_draft_declarations_file() -> dict[str, str]:
+    with open(runfiles.load_path("config/api/draft_declarations.json")) as fs:
+        return json.load(fs)
